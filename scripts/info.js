032f94aed8b2fc0784b9d7057d89c35b09a9de49
nikita <pepkin99@mail.ru>
2022-01-10 19:04:33 +0300

    Добавление и изменение студента

--- scripts/info.js
+++ scripts/info.js
@@ -1,1 +1,6 @@
-alert("Извините, контент пока недоступен.")
+const $buttonHTML = document.querySelector('.body__content__right');
+
+$buttonHTML.addEventListener('click', () =>{
+	alert("Извините, контент пока недоступен.");
+});
+
--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,96 @@
-const $studentsSectionLeft = document.querySelector('.body__content__left');
-const $studentsSectionRight = document.querySelector('.body__content__right');
+class Api{
+	constructor(url, headers){
+		this._url = url;
+		this._headers = headers;
+	}
 
-const $studentsTemplate = document.querySelector('#studentsTemplate').content;
+	getItems(){
+		/* Получаем данные с сервера*/
+		return fetch(this._url, {
+			method: 'GET',
+			headers: this._headers
+		}).then((res) =>{
+			return this._processResult(res, 'Ошибка при получении данных');
+		});
+	}
 
-const deleteItem = (id) => {
-	return fetch(`http://localhost:3000/students/${id}`, {
-		method: 'DELETE',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	}).then((res) =>{
+	deleteItem(id){
+		return fetch(`${this._url}/${id}`, {
+			method: 'DELETE',
+			headers: this._headers
+		}).then((res) =>{
+			return this._processResult(res, 'Ошибка при удалении данных ');
+		});
+	}
+
+	_processResult(res, errorText){
 		if (res.ok){
 			return res.json();
 		}
-		alert('Ошибка при удалении данных ');
-		return Promise.reject();
-	});
+		alert(errorText);
+		return Promise.reject(errorText);
+	}
+
+	createItem(data){
+		return fetch(`${this._url}/`, {
+			method: 'POST',
+			headers: this._headers,
+			body: JSON.stringify(data)
+		}).then((res) =>{
+			return this._processResult(res, 'Ошибка при добавлении записи ');
+		});
+	}
+	
+	updateItem(id, data){
+		return fetch(`${this._url}/${id}`, {
+			method: 'PUT',
+			headers: this._headers,
+			body: JSON.stringify(data)
+		}).then((res) =>{
+			return this._processResult(res, 'Ошибка при изменении записи');
+		});
+	}
 }
+
+class Form {
+   constructor(element) {
+      this._element = element;
+   }
+
+   init(submitHandler, values) {
+      this.closeForm();
+      this._submitHandler = submitHandler;
+      this._element.addEventListener('submit', this._submitHandler);
+
+      if (values) {
+         Object.keys(values).forEach((name) => {
+            this._element.querySelector(`[name=${name}]`).value = values[name];
+         });
+      }
+   }
+
+   closeForm() {
+      this._element.reset();
+      this._element.removeEventListener('submit', this._submitHandler);
+      this._submitHandler = null;
+   }
+}
+
+
+const $studentsSectionLeft = document.querySelector('.body__content__left');
+const $studentsSectionRight = document.querySelector('.body__content__right');
+const $studentsTemplate = document.querySelector('#studentsTemplate').content;
+const $buttonAdd = document.querySelector('.body__container__add');
+const $buttonCancel = document.querySelector('#button_cancel');
+const $buttonOK = document.querySelector('#button_OK');
+const $windowTitle = document.querySelector('.body__window__title');
+const $popup = document.querySelector('#form_student');
+const studentsApi = new Api('http://localhost:3000/students', { 'Content-Type': 'application/json'});
+const studentForm = new Form($popup);
 
 // Получаем данные 
 const renderList = (data) =>{
 
-	$studentsSectionLeft.innerHTML = `<a href="#" style="text-decoration: none;">
-				<div class = "body__container__add">
-					<img src = './img/plus.png' alt = "Add Image" class = "body__image__add">
-					<div class = "body__text__add">
-						<p> Add a student</p>
-					</div>
-				</div>
-			</a>`;
-	$studentsSectionRight.innerHTML = '';
 
 	data.forEach((item) => {
 		if (item.id <= 7) {
@@ -44,26 +105,12 @@ const renderList = (data) =>{
 	});
 }
 
-const getItems = () => {
-/* Получаем данные с сервера*/
-	return fetch('http://localhost:3000/students', {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	}).then((res) =>{
-		if (res.ok){
-			return res.json();
-		}
-		alert('Ошибка при получении данных');
-		return Promise.reject();
-	});
-}
+
 
 const renderObject = (studentsSection, item) =>{
 	const $studentElement = $studentsTemplate.cloneNode(true);
 	const $studentImage = $studentElement.querySelector(".body__student__avatar");
-	const $buttonDelete = $studentElement.querySelector(".body__delete")
+	const $buttonDelete = $studentElement.querySelector(".body__delete");
 
 	$studentElement.querySelector('#name').textContent = item.name;
 	$studentElement.querySelector('#university').textContent = item.university;
@@ -74,12 +121,88 @@ const renderObject = (studentsSection, item) =>{
 
 	$buttonDelete.addEventListener('click', (event) => {
 		event.preventDefault();
-		console.log(item.id)
-		deleteItem(item.id).then(() =>{
-			getItems().then((data) => renderList(data));
+		studentsApi.deleteItem(item.id).then(() =>{
+			event.target.closest('.body__item__frame')?.remove?.();
 		})
 	})
+
+	$studentImage.addEventListener('click', (event)=>{
+		
+		array_name = item.name.split(' ');
+		$windowTitle.textContent = "Редактирование информации о студенте";
+		
+		showPopUp();
+		studentForm.init((event) => {
+			event.preventDefault();
+			const data = {
+				name: $popup.elements[0].value + ' ' + $popup.elements[1].value,
+				university: $popup.elements[2].value,
+				sity: $popup.elements[3].value,
+				url: $popup.elements[4].value
+		};
+
+		studentsApi.updateItem(item.id, data).then(() => {
+			studentsApi.getItems().then((data) => renderList(data));
+			hidePopUp();
+		});
+	}, {
+			name: array_name[0],
+			surname: array_name[1],
+			university: item.university,
+			sity: item.sity,
+			url: item.avatar_url
+		});
+
+	});
 }
 
 
-getItems().then((data) => renderList(data));
+studentsApi.getItems().then((data) => renderList(data));
+
+
+const clear = () =>{
+	document.getElementById("input_name").value = "";
+	document.getElementById("input_surname").value = "";
+	document.getElementById("input_university").value = "";
+	document.getElementById("input_sity").value = "";
+	document.getElementById("input_img").value = "";
+} 
+
+const showPopUp = () =>{
+	$popup.classList.remove('popup__hidden');
+}
+
+const hidePopUp = () =>{
+	$popup.classList.add('popup__hidden');
+	studentForm.closeForm();
+}
+
+$buttonAdd.addEventListener('click', () =>{
+	$windowTitle.textContent = "Добавление информации о студенте";
+	showPopUp();
+	studentForm.init(() =>{
+		const data = {
+			name: $popup.elements[0].value + ' ' + $popup.elements[1].value,
+			university: $popup.elements[2].value,
+			sity: $popup.elements[3].value,
+			avatar_url: $popup.elements[4].value
+		}
+		studentsApi.createItem(data).then((studentData) => {
+			if (studentData.id <= 7) {
+				renderObject($studentsSectionLeft, studentData)	
+			}
+			else if (studentData.id <= 14){
+				renderObject($studentsSectionRight, studentData)	
+			}
+			else{
+				(studentData.id % 2 != 0) ? renderObject($studentsSectionLeft, studentData) : renderObject($studentsSectionRight, studentData)	
+			}
+			clear();
+			hidePopUp();
+		});
+	});
+
+});
+
+
+
